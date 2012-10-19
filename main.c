6fd71f5f244a6a1ecbc60924e40ac0d8cc568180
Lucas Seeber <lucasseeber@gmail.com>
2012-10-19 20:53:32 -0300

    layout2

--- main.c
+++ main.c
@@ -9,20 +9,63 @@ GRUPO 3:
         Seeber, Lucas       lseeber@alu.itba.edu.ar
  */
 
-// TESTEDIT CONFLICT
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "polonet.h"
-
 #include "TFTP_lib.h"
 
+
+#define TFTP_PORT   69
+
+
+#define __SERVER
+#ifdef __SERVER
 int main()
 {
+    TFTPconn* = currentConn;
+
+    if( (currentConn = startTFTPserver(TFTP_PORT)) != NULL)
+    {
+        if(getTFTPconnection(currentConn))
+        {
+
+        }
+    }
+
+
+    // esperar contacto
+
+    // establecer transferencia
+
+    // volver a esperar contacto
+
+    // salir si así se pide
+
+
     return 0;
 }
 
+#elif defined __CLIENT
+int main()
+{
+    // conexión
+    // entrada
+    // transferencia
+    // volver a entrada
 
+
+    return 0;
+}
+#endif
+
+
+#include <time.h>
+void wait(long milliseconds)
+{
+  long timeout = clock() + milliseconds;
+  while( clock() < timeout ) continue;
+}
 
 
